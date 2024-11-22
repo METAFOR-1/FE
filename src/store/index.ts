11ffc37b119ddb7236edbd14e60200f12841266a
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 05:11:17 +0900

    [Feat] set result

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,19 @@
+import { Result } from "@/connection/api/server";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 interface AnalysisStore {
   request?: string;
   setRequest: (request: string) => void;
+  result?: Result;
+  setResult: (result: Result) => void;
 }
 const useAnalysisStore = create(
   persist<AnalysisStore>(
     (set) => ({
       request: undefined,
       setRequest: (request) => set({ request }),
+      result: undefined,
+      setResult: (result) => set({ result }),
     }),
     {
       name: "analysisStore",

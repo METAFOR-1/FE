b47a34c81b668d6d8898b597ea4305932f54824d
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 05:10:33 +0900

    [Chore]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,13 @@ import { useLocation } from "react-router-dom";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const { pathname } = useLocation();
+
+  const footerIgnores = ["/record", "/result"];
   return (
     <div className="font-sf-pro">
       {pathname !== "/record" && <Header />}
       {children}
-      {pathname !== "/record" && <Footer />}
+      {!footerIgnores.includes(pathname) && <Footer />}
     </div>
   );
 }

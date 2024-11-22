c7cce92830a9270e014fba570210668045e7e67c
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 01:58:02 +0900

    [Chore]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div>
+    <div className="font-sf-pro">
       {children}
       <Footer />
     </div>

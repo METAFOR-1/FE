488c860d0a0c2906fe6ae6b0b1f36b1fa543938e
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 01:57:56 +0900

    [Feat] inherit font

--- src/components/root/Page.tsx
+++ src/components/root/Page.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
 import { cn } from "fast-jsx/util";
-import ButtonProvider from "./organism/ButtonProvider";
+import ControllerTemplate from "./template/Controller.template";
 
 export default function RootPage() {
   const container = {
-    displays: "flex items-center justify-center",
+    displays: "flex justify-center",
     sizes: "w-full min-h-screen",
   };
   return (
     <div className={cn(container)}>
-      <ButtonProvider />
+      <ControllerTemplate />
     </div>
   );
 }

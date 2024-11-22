d687e16ab61a2f773ae45a26cd87cf73a35f15aa
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 01:06:33 +0900

    [Fix] fix layout

--- src/components/root/Page.tsx
+++ src/components/root/Page.tsx
@@ -3,25 +3,13 @@ import { cn } from "fast-jsx/util";
 import ButtonProvider from "./organism/ButtonProvider";
 
 export default function RootPage() {
-  const [audioUrl, setAudioUrl] = useState<string>("");
-
   const container = {
     displays: "flex items-center justify-center",
     sizes: "w-full min-h-screen",
   };
-
   return (
     <div className={cn(container)}>
-      <ButtonProvider audioUrlState={[audioUrl, setAudioUrl]} />
-      {audioUrl && (
-        <div>
-          <h3>Recorded Audio:</h3>
-          <audio controls src={audioUrl}></audio>
-          <a href={audioUrl} download="recording.webm">
-            Download Audio
-          </a>
-        </div>
-      )}
+      <ButtonProvider />
     </div>
   );
 }
--- src/components/root/organism/ButtonProvider.tsx
+++ src/components/root/organism/ButtonProvider.tsx
@@ -2,22 +2,32 @@ import { State } from "fast-jsx/interface";
 import recordAudio from "@/util/recordAudio";
 import StartButton from "../molecule/Button";
 import { useRef, useState } from "react";
+import { useMutation } from "react-query";
+import { externalApi } from "@/connection";
 
-export default function ButtonProvider({
-  audioUrlState,
-}: {
-  audioUrlState: State<string>;
-}) {
+export default function ButtonProvider() {
+  const [audioUrl, setAudioUrl] = useState<string>("");
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const audioChunks = useRef<Blob[]>([]);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const { mutate } = useMutation({
+    mutationKey: ["stt"],
+    mutationFn: async (audioUrl: string) => {
+      const response = await fetch(audioUrl);
+      const blob = await response.blob();
+      const file = new File([blob], "recording.mp3", {
+        type: "audio/mp3",
+      });
+      return externalApi.post(file);
+    },
+  });
   if (!isRecording)
     return (
       <StartButton
         onClick={() =>
           recordAudio({
             isRecordingState: [isRecording, setIsRecording],
-            audioUrlState,
+            audioUrlState: [audioUrl, setAudioUrl],
             chunks: audioChunks,
             mediaRecorderRef,
           })
@@ -29,6 +39,7 @@ export default function ButtonProvider({
       onClick={() => {
         mediaRecorderRef.current?.stop();
         setIsRecording(false);
+        mutate(audioUrl);
       }}
     >
       Stop Recording

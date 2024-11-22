3f8e86ca1c413ebff90a0699371c4b34acfe5895
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 05:26:01 +0900

    [Chore]

--- src/connection/axios.ts
+++ src/connection/axios.ts
@@ -2,7 +2,7 @@ import origin from "@/config/origin";
 import { handler } from "axios-wizard";
 
 const httpRequest = handler({
-  api: "https://server.co.kr",
+  api: origin.api,
   external: origin.external,
 });
 

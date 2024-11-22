e8b65645dd3dde519fab6672f8f55167e25a3170
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 01:55:36 +0900

    [Feat] env

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ["**/*.xlsx"],
-  base: "/",
-  resolve: {
-    alias: [
-      { find: "@components", replacement: "/src/components" },
-      { find: "@", replacement: "/src" },
-    ],
-  },
-});
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    assetsInclude: ["**/*.xlsx"],
+    base: "/",
+    resolve: {
+      alias: [
+        { find: "@components", replacement: "/src/components" },
+        { find: "@", replacement: "/src" },
+      ],
+    },
+    define: {
+      "process.env": process.env,
+    },
+  });
+};

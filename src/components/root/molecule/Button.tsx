031d803f94d57c7340e3d0bd72f8837213ecf592
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 01:57:13 +0900

    [Feat] redesign ```StartButton```

--- src/components/root/molecule/Button.tsx
+++ src/components/root/molecule/Button.tsx
@@ -1,15 +1,34 @@
-import { Button } from "fast-jsx";
+import Svg from "@/asset/Svg";
 import { OnClick } from "fast-jsx/interface";
+import { cn } from "fast-jsx/util";
 
-export default function StartButton({ onClick }: { onClick: OnClick }) {
+export default function StartButton({
+  onClick,
+  className,
+}: {
+  onClick: OnClick;
+  className?: string;
+}) {
+  const container = {
+    displays: "flex justify-center items-center",
+    sizes: "w-[367px] h-[367px]",
+    boundaries: "rounded-full",
+    borders: "border-2 border-orange-500",
+    shadow: "shadow-md shadow-gray-500",
+    styles: "cursor-pointer",
+    className,
+  };
+  const body = {
+    displays: "flex justify-center items-center",
+    sizes: "w-[218px] h-[218px]",
+    boundaries: "rounded-full",
+    backgrounds: "bg-orange-500",
+  };
   return (
-    <Button
-      title="Start Recording"
-      onClick={onClick}
-      option={{
-        background: "bg-green-dark",
-        boundary: "rounded-full",
-      }}
-    />
+    <button className={cn(container)} onClick={onClick}>
+      <div className={cn(body)}>
+        <Svg.Icon.Mic />
+      </div>
+    </button>
   );
 }

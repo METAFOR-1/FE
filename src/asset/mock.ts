f57d1ddb211ed176ee60a19c265076f9291960be
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 05:10:42 +0900

    [Chore] set assets

---
+++ src/asset/mock.ts
@@ -0,0 +1,12 @@
+import { Result } from "@/connection/api/server";
+
+const result: Result = {
+  muscleName: "실전압축근육",
+  youtubeTitle:
+    "[vlog] #먼치킨 #새끼고양이와 #포메라니안 강아지 같이 키우기 - 합사 첫날부터 친해지기까지",
+  youtubeLink: "https://youtu.be/jANE8lpoj2c?si=rfGlpIDxaV6H-J1n",
+  reason: "포메라니안 강아지와 새끼 고양이를 함께 키우는 vlog",
+  imageLink: "/images/cat.jpeg",
+};
+
+export { result };

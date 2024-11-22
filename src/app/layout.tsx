c5c56b5b9ac24ee7517e4ec9f5793c825612391f
minhyeong <minhyeong.dev@gmail.com>
2024-11-23 02:03:01 +0900

    [Fix] exception handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/design/Footer";
 import { ReactNode } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const { pathname } = useLocation();
   return (
     <div className="font-sf-pro">
       {children}
-      <Footer />
+      {pathname !== "/" && <Footer />}
     </div>
   );
 }
